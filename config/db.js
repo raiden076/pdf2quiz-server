9dba83aa7a5bd39cdb61e1053fb79d22ab29cd82
arkaprav0 <mail@arkaprav0.in>
2025-04-24 16:43:23 +0530

    setup db

---
+++ config/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
+
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+};
+
+const connectDB = async () => {
+  if (!uri) {
+    console.error('MONGODB_URI is not defined');
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(uri, clientOptions);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error connecting to mongodb: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

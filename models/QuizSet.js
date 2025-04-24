d6fe4119315074899028d5aa680c243711b389e0
arkaprav0 <mail@arkaprav0.in>
2025-04-24 16:43:07 +0530

    minor refactor models

--- models/QuizSet.js
+++ models/QuizSet.js
@@ -1,60 +1,67 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // structure of individual questions
-const questionSchema = new mongoose.Schema({
+const questionSchema = new mongoose.Schema(
+  {
     questionText: {
-        type: String,
-        required: [true, "Question text is required"],
-        trim: true
+      type: String,
+      required: [true, 'Question text is required'],
+      trim: true,
     },
     options: {
-        type: [String],
-        required: [true, "Options are required"],
-        trim: true,
-        validate: [arr => arr.length >= 1, "At least 2 options are required"]
+      type: [String],
+      required: [true, 'Options are required'],
+      trim: true,
+      validate: [(arr) => arr.length >= 1, 'At least 2 options are required'],
     },
     correctAnswerIndex: {
-        type: Number,
-        required: [true, "Correct answer index is required"],
-        min: 0
-    }
-}, { _id: false });
+      type: Number,
+      required: [true, 'Correct answer index is required'],
+      min: 0,
+    },
+  },
+  { _id: false },
+);
 
 // a quiz set comprised of multiple questions created by a user from a pdf file
 const quizSetSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, "User ID is required"],
-        ref: "User",
-        index: true
-    },
-    pdfFileName: {
-        type: String,
-        required: [true, "PDF file name is required"],
-        trim: true
-    },
-    pdfUrl: {
-        type: String,
-        required: [true, "PDF URL is required"],
-    },
-    questions: {
-        type: [questionSchema],
-        required: [true, "Questions are required"],
-        validate: [arr => arr.length >= 1, "At least 1 question is required"]
-    },
-    // not sure this will be needed in the mvp, but let's keep it for now
-    status: {
-        type: String,
-        required: true,
-        enum: ['processing', 'ready', 'error'],
-        default: 'processing',
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, 'User ID is required'],
+    ref: 'User',
+    index: true,
+  },
+  pdfFileName: {
+    type: String,
+    required: [true, 'PDF file name is required'],
+    trim: true,
+  },
+  pdfUrl: {
+    type: String,
+    required: [true, 'PDF URL is required'],
+  },
+  questions: {
+    type: [questionSchema],
+    required: [true, 'Questions are required'],
+    validate: [(arr) => arr.length >= 1, 'At least 1 question is required'],
+  },
+  // not sure this will be needed in the mvp, but let's keep it for now
+  status: {
+    type: String,
+    required: true,
+    enum: ['processing', 'ready', 'error'],
+    default: 'processing',
+  },
+  errorMessage: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // create and export quizset model
-const QuizSet = mongoose.model("QuizSet", quizSetSchema);
+const QuizSet = mongoose.model('QuizSet', quizSetSchema);
 module.exports = QuizSet;
--- models/User.js
+++ models/User.js
@@ -1,49 +1,48 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 // User Schema
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
-
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minlength: [6, 'Password must be at least 6 characters long'],
-        select: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        select: false
-    }
-})
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    select: false,
+  },
+});
 
 // userschema pre save middleware to hash password
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
-    }
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
-})
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 // userschema method to check plaintxt password against hashed password
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
-}
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 // create and export user model
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
